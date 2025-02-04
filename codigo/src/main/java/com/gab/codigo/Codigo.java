cc473bd7ad388842306911b6c04820335962afd5
Gabriel Assis de Souza <gabriel.souza.1523286@sga.pucminas.br>
2025-02-04 13:08:22 -0300

    Adicionando telas do sistema

---
+++ codigo/src/main/java/com/gab/codigo/Codigo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gab.codigo;
+
+/**
+ *
+ * @author eduar
+ */
+public class Codigo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
